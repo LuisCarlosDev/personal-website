dde6e2dc1afe4985badcec85b7af1087d0269370
Luis Carlos Silva <luis.silva@smgbit.com.br>
2024-06-02 15:11:58 -0300

    initial header and animation logo

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,7 +1,22 @@
+import Link from 'next/link'
+
 export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="px-[120px] py-8 flex items-center justify-between select-none">
+      <h1 className="gradient-text uppercase font-bold text-transparent animate-gradient">
+        Luis Carlos
+      </h1>
+
+      <nav className="space-x-4">
+        <Link href="/">
+          <span className="text-primary mr-2">#</span>
+          Home
+        </Link>
+        <Link href="/">
+          <span className="text-primary mr-2">#</span>
+          Projetos
+        </Link>
+      </nav>
     </header>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from './_components/header'
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full flex flex-col">
+    <div className="min-h-screen w-full flex flex-col max-w-[1600px]">
       <Header />
       <div>
         <h1>Hello world</h1>
